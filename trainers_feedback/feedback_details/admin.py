38b12fea72d2231c356eafe9ddc558ef11a642bf
Anandhan <anandhangithub@gmail.com>
2023-01-18 14:55:41 +0530

    databases add

--- trainers_feedback/feedback_details/admin.py
+++ trainers_feedback/feedback_details/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from feedback_details.models import trainer_data
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(trainer_data)
+# admin.site.register(trainer_feedback)
--- trainers_feedback/feedback_details/models.py
+++ trainers_feedback/feedback_details/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class trainer_data(models.Model):
+    id = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    destination  = models.CharField(max_length=30)
+    phone_no = models.IntegerField()
+
+    def __str__(self):
+        return self.id
+
+# class trainer_feedback(models.Model):
+#     id = models.OneToOneField(
+#         trainer_data,
+#         on_delete=models.CASCADE,
+#         primary_key=True,
+#         )
+#     communicatin_skill = models.IntegerField()
+#     content_delivered = models.IntegerField()
+#     doubt_clarification = models.IntegerField()
+#     technical_skill = models.IntegerField()
+#     feedback = models.CharField(max_length = 500)
