1b1e26d9883cd6aee6b9d3d8b6f86bf1e4edf5a5
AmudanThangavel <amudanthangavel@gmail.com>
2023-01-18 16:14:41 +0530

    adding views

--- trainers_feedback/feedback_details/views.py
+++ trainers_feedback/feedback_details/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from feedback_details.models import trainer_data, trainer_feedback
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello")
+    trainer_list = trainer_data.objects.all()
+    average_score = []
+
+    for trainer in trainer_list:
+        feed_details = trainer_feedback.objects.filter(train_id=trainer.id)
+        average = 0
+        for feed in feed_details:
+            average += feed.communicatin_skill + feed.content_delivered + \
+                feed.doubt_clarification + feed.technical_skill
+        average_score.append(average)
+
+    return render(request)
